76df1b91ff453062d0591f8cb5e3a69e0319638e
oscar anillo <oscarani14@gmail.com>
2022-09-05 19:36:45 -0500

    add section a className and a background

--- src/components/main-card.js
+++ src/components/main-card.js
@@ -1,13 +1,22 @@
 export default function MainCard(){
     return (
-        <div className="main-card"> 
-            <div>
-                <img src="/images/image-jeremy.png" alt="" />
+        <section className="section-card">
+            <div className="main-card"> 
+                <div className="avatar">
+                    <img src="/images/image-jeremy.png" alt="" />
+                </div>
+                <div className="avatar-text">
+                    <p>Report for</p>
+                    <h1>Jeremy Robson</h1>
+                </div>
             </div>
-            <div>
-                <p>Report for</p>
-                <h1>Jeremy Robson</h1>
-            </div>
-        </div>
+            <nav className="main-nav">
+                <ul>
+                    <li>Daily</li>
+                    <li>Weekly</li>
+                    <li>Monthly</li>
+                </ul>
+            </nav>
+        </section>
     )
 }
