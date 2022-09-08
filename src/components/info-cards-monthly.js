466e151b6745d57d52f8440559458e4b497772da
oscar anillo <oscarani14@gmail.com>
2022-09-08 16:13:14 -0500

    add animation to infoCards

--- src/components/info-cards-monthly.js
+++ src/components/info-cards-monthly.js
@@ -1,6 +1,23 @@
+import { useEffect } from "react";
+import { gsap } from 'gsap'
 import data from '../data/data.json';
 
 export default function InfoCardsMonthly(){
+    useEffect(() => {
+        gsap.fromTo('.cards', {
+            x: 100,
+            stagger: 0.1,
+            opacity: 0,
+            duration: 1,
+            ease: 'expo'
+        }, {
+            x: 0,
+            stagger: 0.1,
+            opacity: 1,
+            duration: 1,
+            ease: 'expo'
+        })
+    }, []);
     return (
         <div className='cards-desktop'>
             {data.map(item => (
